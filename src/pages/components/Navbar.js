73fd21c54517ad136d5cc5cc8f2360276ef10006
mhmd-fl <mohammed@founderandlightning.com>
2023-04-14 18:22:46 +0530

    removed logo

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -4,7 +4,6 @@ function Navbar() {
     return (
         <div>
             <nav>
-                <img src="https://buildyourinnovation.com/wp-content/uploads/2020/11/byi.png" width="300" height="90" alt='' />
             </nav>
         </div>
     )
